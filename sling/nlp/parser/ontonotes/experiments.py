1c7d7cb35ec58e6e713503df97b9399d0b7fd048
Cezar Sas <cezar.sas@gmail.com>
2019-02-06 10:25:40 +0100

    Added experiments

--- sling/nlp/parser/ontonotes/experiments.py
+++ sling/nlp/parser/ontonotes/experiments.py
@@ -24,7 +24,7 @@ EXPERIMENTS = {
   },
 
   "es_wiki_5000_conll_0": {
-    "train": [("wiki_srl_es.gold_conll", 5000), ("CoNLL2009-ST-Spanish-train.gold_conll", 0)],
+    "train": [("wiki_srl_es.gold_conll", 5000)],
     "dev": ["CoNLL2009-ST-Spanish-development.gold_conll"],
     "test": ["CoNLL2009-ST-evaluation-Spanish.gold_conll"]
   },
